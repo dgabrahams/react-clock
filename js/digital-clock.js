8c699d03d3c3c40a3484562e7c352c5d7cb7327c
Daniel Abrahams <daniel.abrahams@cohaesus.co.uk>
2017-07-28 08:48:04 +0100

    Addition of basic timezone selector

--- js/digital-clock.js
+++ js/digital-clock.js
@@ -46,10 +46,12 @@ var DefaultClock = React.createClass({
             // get time that is required to be + or -
             //+ or - that form current time and set as the state value.
 
-            console.log('currentTime before: '+currentTime);
-            currentTime = moment( currentTime.toISOString() ).tz( String(this.state.timeZone) ).format();
-            console.log('currentTime new: '+currentTime);
-            // console.log('currentTime new: '+currentTime.toISOString() );
+
+            // console.log('currentTime before: '+currentTime);
+            // currentTime = moment( currentTime.toISOString() ).tz( String(this.state.timeZone) ).format();
+            // console.log('currentTime new: '+currentTime);
+            // // console.log('currentTime new: '+currentTime.toISOString() );
+
 
             //require this format to set the timzezone of a new date: Wed Mar 25 2015 09:56:24 GMT+0500
             //currentTime = new Date(currentTime);
