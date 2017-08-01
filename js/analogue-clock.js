c99a172a566c6d4141d016308a0fbe5f1961f3de
Daniel Abrahams <daniel.abrahams@cohaesus.co.uk>
2017-08-01 09:53:06 +0100

    Addition of timezone calculation into main component and analogue clock theme component

--- js/analogue-clock.js
+++ js/analogue-clock.js
@@ -50,9 +50,13 @@ var AnalogueClock = React.createClass({
 
         //also, as long as the ntp time is known to a second, then any seconds after that time can be counted, allowing correct setting of time.?
 
-        var hours = currentTime.getHours();
-        var minutes = currentTime.getMinutes();
-        var seconds = currentTime.getSeconds();
+        // var hours = currentTime.getHours();
+        // var minutes = currentTime.getMinutes();
+        // var seconds = currentTime.getSeconds();
+
+        var hours = currentTime.getUTCHours();
+        var minutes = currentTime.getUTCMinutes();
+        var seconds = currentTime.getUTCSeconds();
 
         if ( this.state.timeZoneValue !== null ) {
             console.log('this.state.timeZoneValue !== null');
